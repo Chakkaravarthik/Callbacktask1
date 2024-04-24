e7af6e5a0710bd7395a7d44505bb1491a91afb2b
Chakkaravarthik <152775343+Chakkaravarthik@users.noreply.github.com>
2024-04-24 22:23:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const count = document.querySelector(".count");
+
+function callback(){
+
+
+
+setTimeout(() => {
+  count.innerHTML = `<h1>10</h1>`; 
+  setTimeout(() => {
+    count.innerHTML = `<h1>9</h1>`; 
+    setTimeout(() => {
+      count.innerHTML = `<h1>8</h1>`; 
+      setTimeout(() => {
+        count.innerHTML = `<h1>7</h1>`; 
+        setTimeout(() => {
+          count.innerHTML = `<h1>6</h1>`; 
+          setTimeout(() => {
+            count.innerHTML = `<h1>5</h1>`; 
+            setTimeout(() => {
+              count.innerHTML = `<h1>4</h1>`; 
+              setTimeout(() => {
+                count.innerHTML = `<h1>3</h1>`; 
+                setTimeout(() => {
+                  count.innerHTML = `<h1>2</h1>`; 
+                  setTimeout(() => {
+                    count.innerHTML = `<h1>1</h1>`; 
+                    setTimeout(() => {
+                      count.innerHTML = `<h1>Happy Independence Day!!</h1>`;
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+}
